f69bff029f07ad6fd998d4da8996f33d6ee13a1b
Benedikt Meurer <bmeurer@google.com>
2018-12-12 09:45:35 +0100

    HTTP

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -30,4 +30,8 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
 }
